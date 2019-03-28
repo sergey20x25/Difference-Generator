d9a36b2f2336f1fbc15d7e18cc99bc477819c18e
sergey20x25 <vdoond@mail.ru>
2019-03-28 16:50:49 +0700

    add ini parse function

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 install:
 	npm install
 start:
-	npx babel-node src/bin/gendiff.js '__tests__/__fixtures__/beforeRec.json' '__tests__/__fixtures__/afterRec.json'
+	npx babel-node src/bin/gendiff.js '__tests__/__fixtures__/beforeRec.ini' '__tests__/__fixtures__/afterRec.ini'
 publish:
 	npm publish
 lint:
--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,24 @@
 import { safeLoad } from 'js-yaml';
 import { parse } from 'ini';
 
+const parseIni = (iniData) => {
+  const rawParsed = parse(iniData);
+  const stringifyKeysAndValues = (object) => {
+    return Object.keys(object).reduce((acc, key) => {
+      const strKey = String(key);
+      const newValue = object[key] instanceof Object
+        ? stringifyKeysAndValues(object[key]) : String(object[key]);
+      return { ...acc, [strKey]: newValue };
+    }, {});
+  };
+  return stringifyKeysAndValues(rawParsed);
+};
+
 const parsers = {
   '.json': JSON.parse,
   '.yml': safeLoad,
   '.yaml': safeLoad,
-  '.ini': parse,
+  '.ini': parseIni,
 };
 
 export default (fileExt, data) => parsers[fileExt](data);
