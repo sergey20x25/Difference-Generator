12b082056c65ea800b8d1d8ddccea38dab873d14
sergey20x25 <vdoond@mail.ru>
2019-03-28 18:23:14 +0700

    fix lint problem in parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -3,14 +3,12 @@ import { parse } from 'ini';
 
 const parseIni = (iniData) => {
   const rawParsed = parse(iniData);
-  const stringifyKeysAndValues = (object) => {
-    return Object.keys(object).reduce((acc, key) => {
-      const strKey = String(key);
-      const newValue = object[key] instanceof Object
-        ? stringifyKeysAndValues(object[key]) : String(object[key]);
-      return { ...acc, [strKey]: newValue };
-    }, {});
-  };
+  const stringifyKeysAndValues = object => Object.keys(object).reduce((acc, key) => {
+    const strKey = String(key);
+    const newValue = object[key] instanceof Object
+      ? stringifyKeysAndValues(object[key]) : String(object[key]);
+    return { ...acc, [strKey]: newValue };
+  }, {});
   return stringifyKeysAndValues(rawParsed);
 };
 
