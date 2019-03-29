fb32abb1f9946d99af674aeb8fd6b9cefec46068
sergey20x25 <vdoond@mail.ru>
2019-03-29 21:25:32 +0700

    fix linter problem

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,11 +1,9 @@
 import renderTree from './rendererTree';
 import renderPlain from './rendererPlain';
-import renderJson from './rendererJson';
 
 const renderers = {
   tree: renderTree,
   plain: renderPlain,
-  json: renderJson,
 };
 
 export default (astData, format) => renderers[format](astData);
